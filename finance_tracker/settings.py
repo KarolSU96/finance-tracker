cc24648c9d727a700de60c2fcb3de2cc22a307ac
KarolSU96 <>
2023-10-25 17:43:18 +0000

    Ready for deployment

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-karolsu96-finance-tracke-jxuj07couj.us2.codeanyapp.com', 'financial-plannero.herokuapp.com']
+ALLOWED_HOSTS = ['8000-karolsu96-finance-tracke-jxuj07couj.us2.codeanyapp.com',
+                 'financial-plannero.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,7 +133,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL= '/media/'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
