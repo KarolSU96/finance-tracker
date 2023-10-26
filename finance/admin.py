562c226d89ee5a8971ca081e9f6dd1f3399b0b21
KarolSU96 <>
2023-10-26 11:59:33 +0000

    Fix prepupulated_fields in PlanAdmin and add list:filter in both PlanAdmin and TransactionAdmin

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Plan, Transaction, Category
 
-# Register your models here.
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'budget', 'slug', 'created_on')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('created_on',)
+    search_fields = ['name']
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('plan', 'category', 'amount', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['description', 'category']
+
+
+admin.site.register(Category)
--- finance/models.py
+++ finance/models.py
@@ -17,19 +17,28 @@ class Plan(models.Model):
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
 class Transaction(models.Model):
-    plan = models.ForeignKey(Plan, null=True, blank=True, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete= models.CASCADE)
+    plan = models.ForeignKey(
+        Plan, null=True, blank=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering= ['-created_on']
+        ordering = ['-created_on']
 
     def __str__(self):
         return f"{self.plan.user.username} - {self.plan.name} - {self.category.name} - €{self.amount} - {self.date.strftime('%Y-%m-%d')} "
