f15608415ca40ccc0f221d954a08f7de095549cc
KarolSU96 <>
2023-11-07 16:59:09 +0000

    Add comments to script.js. Corrected styling in edit_transaction template

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,14 @@
+// Function waits for DOM to be fully loaded before execution
 document.addEventListener('DOMContentLoaded', function() {
-
+    // get element containing messages
     const messages = document.querySelector('.messages')
 
-
+    // check if messages exist
     if (messages) {
         // Display the first message with SweetAlert
         const firstMessage = messages.querySelector('div');
         if (firstMessage) {
+            // Use SweetAlert to show notification
             Swal.fire({
                 title: firstMessage.textContent,
                 icon: firstMessage.classList.contains('success') ? 'success' : 'error',
